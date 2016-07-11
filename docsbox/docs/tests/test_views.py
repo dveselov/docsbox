0409315e3e62788e6a808186baa318a937793200
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-07-11 03:21:26 +0300

    Add DocumentCreateView test case

--- docsbox/docs/tests/test_views.py
+++ docsbox/docs/tests/test_views.py
@@ -49,3 +49,101 @@ class DocumentViewTestCase(BaseTestCase):
         self.assertEqual(ujson.loads(response.data), {
             "message": "Unknown task_id"
         })
+
+class DocumentCreateViewTestCase(BaseTestCase):
+
+    def test_submit_without_file(self):
+        response = self.client.post("/api/v1/", data={
+            "options": ujson.dumps(["pdf"])
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "file field is required"
+        })
+
+    def test_submit_invalid_mimetype(self):
+        response = self.submit_file("/bin/sh", {
+            "formats": ["pdf"],
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "Not supported mimetype: 'application/x-sharedlib'"
+        })
+
+    def test_submit_empty_formats(self):
+        filename = os.path.join(self.samples, "sample.docx")
+        response = self.submit_file(filename, {
+            "formats": []
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "Invalid 'formats' value"
+        })
+
+    def test_submit_invalid_formats(self):
+        filename = os.path.join(self.samples, "sample.docx")
+        response = self.submit_file(filename, {
+            "formats": ["csv"]
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "'application/vnd.openxmlformats-offi"
+                       "cedocument.wordprocessingml.document'"
+                       " mimetype can't be converted to 'csv'"
+        })
+
+    def test_submit_invalid_thumbnails_field(self):
+        filename = os.path.join(self.samples, "sample.docx")
+        response = self.submit_file(filename, {
+            "formats": ["pdf"],
+            "thumbnails": 1,
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "Invalid 'thumbnails' value"
+        })
+
+    def test_submit_invalid_thumbnails_size_field(self):
+        filename = os.path.join(self.samples, "sample.docx")
+        response = self.submit_file(filename, {
+            "formats": ["pdf"],
+            "thumbnails": {
+                "size": None,
+            },
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "Invalid 'size' value"
+        })
+
+    def test_submit_invalid_thumbnails_size_field_value(self):
+        filename = os.path.join(self.samples, "sample.docx")
+        response = self.submit_file(filename, {
+            "formats": ["pdf"],
+            "thumbnails": {
+                "size": "ZZx43",
+            },
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json, {
+            "message": "Invalid 'size' value"
+        })
+
+    def test_submit_valid_thumbnails(self):
+        filename = os.path.join(self.samples, "sample.docx")
+        response = self.submit_file(filename, {
+            "formats": ["pdf"],
+            "thumbnails": {
+                "size": "640x480",
+            },
+        })
+        json = ujson.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.get("status"), "queued")
